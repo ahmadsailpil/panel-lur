a7f37942e13b1dcfb409c63f4a9cc371c75271ce
dawawasgg <106122054+dawawasgg@users.noreply.github.com>
2022-12-14 11:15:05 +0800

    Update changelog_parse.py

--- scripts/changelog_parse.py
+++ scripts/changelog_parse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-__author__ = "Vilhelm Prytz"
-__email__ = "vilhelm@prytznet.se"
+__author__ = "Ridho Bot"
+__email__ = "ohlx@gmail.com"
 
 def read():
     with open("CHANGELOG.md") as f:
